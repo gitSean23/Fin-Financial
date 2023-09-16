9cf5bc68a80a1b03ad8b14abf36f86875dc4ee87
StudentJamesChen <jameselichen@gmail.com>
2023-09-16 15:26:03 -0400

    Chain.py functionalized.

--- chain.py
+++ chain.py
@@ -17,47 +17,49 @@ os.environ["OPENAI_API_KEY"] = open_api_key
 os.environ["PINECONE_API_KEY"] = pinecone_api_key
 os.environ["PINECONE_ENV"] = pinecone_env
 
-# Load
-loader = PyPDFLoader("pdf\Vanguard_guide_to_financial_wellness.pdf")
-rawDocs = loader.load()
 
-# Splitting
-text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 1000,
-        chunk_overlap = 200,
-)
-docs = text_splitter.split_documents(rawDocs)
+def create_qa_bot(query):
+    # Load
+    loader = PyPDFLoader("pdf\Vanguard_guide_to_financial_wellness.pdf")
+    rawDocs = loader.load()
 
-# Bring in the embedding API
-embeddings = OpenAIEmbeddings()
+    # Splitting
+    text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 1000,
+            chunk_overlap = 200,
+    )
+    docs = text_splitter.split_documents(rawDocs)
 
-# Initialize the pinecone api and environment
-pinecone.init(
-    api_key=os.getenv("PINECONE_API_KEY"),  # find at app.pinecone.io
-    environment=os.getenv("PINECONE_ENV"),  # next to api key in console
-)
+    # Bring in the embedding API
+    embeddings = OpenAIEmbeddings()
 
-# Create the index if needed.
-index_name = "vanguard-guide"
-if index_name not in pinecone.list_indexes():
-    # we create a new index
-    pinecone.create_index(
-      name=index_name,
-      metric='cosine',
-      dimension=1536  
-)
+    # Initialize the pinecone api and environment
+    pinecone.init(
+        api_key=os.getenv("PINECONE_API_KEY"),  # find at app.pinecone.io
+        environment=os.getenv("PINECONE_ENV"),  # next to api key in console
+    )
 
+    # Create the index if needed.
+    # The OpenAI embedding model `text-embedding-ada-002 uses 1536 dimensions`
+    index_name = "vanguard-guide"
+    if index_name not in pinecone.list_indexes():
+        # we create a new index
+        pinecone.create_index(
+        name=index_name,
+        metric='cosine',
+        dimension=1536  
+    )
 
-# The OpenAI embedding model `text-embedding-ada-002 uses 1536 dimensions`
-docsearch = Pinecone.from_existing_index(index_name, embeddings)
+    # Find the doc from the index.
+    docsearch = Pinecone.from_existing_index(index_name, embeddings)
+    relevent_docs = docsearch.similarity_search(query)
 
-query = "Why financial wellness?"
-docs = docsearch.similarity_search(query)
+    # Langchain to answer the query with the relevant docs.
+    llm = ChatOpenAI(temperature=0, openai_api_key=open_api_key, model_name='gpt-4')
+    chain = load_qa_chain(llm, chain_type="stuff")
+    relevent_docs = docsearch.similarity_search(query)
+    answer = chain.run(input_documents=relevent_docs, question=query)
 
-llm = ChatOpenAI(temperature=0, openai_api_key=open_api_key, model_name='gpt-4')
+    # Answer plaintext is returned.
+    return answer
 
-chain = load_qa_chain(llm, chain_type="stuff")
-
-docs = docsearch.similarity_search(query)
-
-print(chain.run(input_documents=docs, question=query))
