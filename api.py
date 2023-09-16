95d5e870d7b505db6c488e8824503aede71ab076
Sean Feldman <the.feldman23@gmail.com>
2023-09-16 01:27:33 -0400

    The bot is yapping

---
+++ api.py
@@ -0,0 +1,7 @@
+# get a token: https://platform.openai.com/account/api-keys
+
+# from getpass import getpass
+
+# OPENAI_API_KEY = getpass()
+
+apikey = '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'
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+# IF anything goes wrong and there is no explanation for it, it's prob the API KEY
 import streamlit as st
-# import openai
-from langchain.llms import VertexAI
+import os
+from api import apikey
+os.environ["OPENAI_API_KEY"] = apikey
+import openai
+
 
 st.title("Fin")
 if "messages" not in st.session_state:
@@ -9,3 +13,17 @@ if "messages" not in st.session_state:
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+#     if not openai_api_key:
+#         st.info("Please add your OpenAI API key to continue.")
+#         st.stop()
+
+    # openai.api_key = openai_api_key
+
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
