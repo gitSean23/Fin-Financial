767d2b011d562adce1d10df233b6e11574cba538
Sean Feldman <the.feldman23@gmail.com>
2023-09-17 09:41:23 -0400

    env variables added

--- chain.py
+++ chain.py
@@ -1,7 +1,4 @@
 # pdf code
-from api import open_api_key
-from api import pinecone_api_key
-from api import pinecone_env
 import openai
 import os
 import getpass
@@ -13,54 +10,39 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
+from dotenv import load_dotenv
 
-os.environ["OPENAI_API_KEY"] = open_api_key
-os.environ["PINECONE_API_KEY"] = pinecone_api_key
-os.environ["PINECONE_ENV"] = pinecone_env
+# OPENAI_API_KEY = config('OPENAI_API_KEY')
+# PINECONE_API_KEY = config('PINECONE_API_KEY')
+# PINECONE_ENV = config('PINECONE_ENV')
+
+load_dotenv()
 
 
 def create_qa_bot(query):
-    # Load
-    # loader = PyPDFLoader("pdf/Vanguard_guide_to_financial_wellness.pdf")
-    # rawDocs = loader.load()
-
-    # # Splitting
-    # text_splitter = RecursiveCharacterTextSplitter(
-    #         chunk_size = 1000,
-    #         chunk_overlap = 200,
-    # )
-    # docs = text_splitter.split_documents(rawDocs)
 
     # Bring in the embedding API
     embeddings = OpenAIEmbeddings()
 
     # Initialize the pinecone api and environment
     pinecone.init(
-        api_key=os.getenv("PINECONE_API_KEY"),  # find at app.pinecone.io
-        environment=os.getenv("PINECONE_ENV"),  # next to api key in console
+        api_key=os.environ['PINECONE_API_KEY'],  # find at app.pinecone.io
+        environment=os.environ['PINECONE_ENV'],  # next to api key in console
     )
 
-    # Create the index if needed.
     # The OpenAI embedding model `text-embedding-ada-002 uses 1536 dimensions`
     index_name = "vanguard-guide"
-    # if index_name not in pinecone.list_indexes():
-    #     # we create a new index
-    #     pinecone.create_index(
-    #     name=index_name,
-    #     metric='cosine',
-    #     dimension=1536  
-    # )
 
     # Find the doc from the index.
     docsearch = Pinecone.from_existing_index(index_name, embeddings)
     relevent_docs = docsearch.similarity_search(query)
 
     # Langchain to answer the query with the relevant docs.
-    llm = ChatOpenAI(temperature=0, openai_api_key=open_api_key, model_name='gpt-4')
+    llm = ChatOpenAI(temperature=0, openai_api_key=os.environ['OPENAI_API_KEY'], model_name='gpt-4')
     chain = load_qa_chain(llm, chain_type="stuff")
     relevent_docs = docsearch.similarity_search(query)
     answer = chain.run(input_documents=relevent_docs, question=query)
-
+    print(answer)
     # Answer plaintext is returned.
     return answer
 
--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-# IF anything goes wrong and there is no explanation for it, it's prob the API KEY
 import streamlit as st
 import os
-from api import open_api_key
-os.environ["OPENAI_API_KEY"] = open_api_key
 import openai
-from chain import create_qa_bot
-from api import eleven_api_key
-os.environ["ELEVEN_API_KEY"] = eleven_api_key
 from elevenlabs import generate, play, set_api_key
+from chain import create_qa_bot
+from dotenv import load_dotenv
 
-set_api_key(eleven_api_key)
+# openai_api_key = config('OPENAI_API_KEY')
+# ELEVEN_API_KEY = config('ELEVEN_API_KEY')
+
+load_dotenv()
+
+openai_api_key = os.environ['OPENAI_API_KEY']
+set_api_key(os.environ['ELEVEN_API_KEY'])
 
 st.set_page_config(page_title="FinAI", page_icon="🐬")
 
-st.title("Fin")
+st.title("FinAI 🐬")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "Let's get yo money up! Start by asking me the basics."}]
 
@@ -22,30 +24,23 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
-#     if not openai_api_key:
-#         st.info("Please add your OpenAI API key to continue.")
-#         st.stop()
-
-    # openai.api_key = openai_api_key
 
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
     with st.spinner("Thinking.."):
-        responseRole = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-        responseRole1 = responseRole.choices[0].message
         response = create_qa_bot(prompt)
-        newMsg = {"role": responseRole1["role"], "content": response}
+        newMsg = {"role": "assistant", "content": response}
         
         st.session_state.messages.append(newMsg)
 
         st.chat_message("assistant").write(newMsg["content"])
         
-    audio = generate(
-        text=response,
-        voice="Bella",
-        model="eleven_multilingual_v2",
-        )
+    # audio = generate(
+    #     text=response,
+    #     voice="Bella",
+    #     model="eleven_multilingual_v2"
+    #     )
 
-    play(audio)
+    # play(audio)
 
